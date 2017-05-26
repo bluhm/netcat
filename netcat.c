df39324a9e616d69f000d9ce0af4d6a127dc7950
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-05-26 18:05:42 +0200

    cvs commit

--- netcat.c
+++ netcat.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: netcat.c,v 1.182 2017/05/10 21:56:53 bluhm Exp $ */
+/* $OpenBSD: netcat.c,v 1.183 2017/05/26 16:05:35 bluhm Exp $ */
 /*
  * Copyright (c) 2001 Eric Jackson <ericj@monkey.org>
  * Copyright (c) 2015 Bob Beck.  All rights reserved.
