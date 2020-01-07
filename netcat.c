da99d475a58f232e324d3277487460115cac1415
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-01-07 15:12:35 +0100

    If a cert hash is expected and there is none, TLS must fail.

--- netcat.c
+++ netcat.c
@@ -835,8 +835,8 @@ tls_setup_client(struct tls *tls_ctx, int s, char *host)
 	}
 	if (vflag)
 		report_tls(tls_ctx, host);
-	if (tls_expecthash && tls_peer_cert_hash(tls_ctx) &&
-	    strcmp(tls_expecthash, tls_peer_cert_hash(tls_ctx)) != 0)
+	if (tls_expecthash && (tls_peer_cert_hash(tls_ctx) == NULL ||
+	    strcmp(tls_expecthash, tls_peer_cert_hash(tls_ctx)) != 0))
 		errx(1, "peer certificate is not %s", tls_expecthash);
 	if (Zflag) {
 		save_peer_cert(tls_ctx, Zflag);
@@ -865,8 +865,8 @@ tls_setup_server(struct tls *tls_ctx, int connfd, char *host)
 		if ((TLSopt & TLS_CCERT) && !gotcert)
 			warnx("No client certificate provided");
 		else if (gotcert && tls_expecthash &&
-		    tls_peer_cert_hash(tls_cctx) &&
-		    strcmp(tls_expecthash, tls_peer_cert_hash(tls_cctx)) != 0)
+		    (tls_peer_cert_hash(tls_cctx) == NULL ||
+		    strcmp(tls_expecthash, tls_peer_cert_hash(tls_cctx)) != 0))
 			warnx("peer certificate is not %s", tls_expecthash);
 		else if (gotcert && tls_expectname &&
 		    (!tls_peer_cert_contains_name(tls_cctx, tls_expectname)))
